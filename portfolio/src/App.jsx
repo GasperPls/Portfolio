ec94c68b432b271f8e5fb56eabe32a4fb83f3280
GasperPls <rouni.assaf@outlook.com>
2026-01-15 21:07:03 -0800

    Footer work and NavBar startup

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,21 +1,8 @@
-
-import './App.css'
-
-function Card() {
-  return (
-    <h2>Card Component</h2>
-  )
-}
-
 function App() {
   return (
-    <div>
-      <h1>Test 1</h1>
-      <Card />
-      <Card />
-      <Card />
+    <div className='App'>
+      <h1>Hello World!</h1>
     </div>
-
   )
 }
 
