bb5dc16ddcf39afaefe80d919e5805cd8aa6c4a4
GasperPls <rouni.assaf@outlook.com>
2026-01-15 21:14:25 -0800

    Footer work and Nav Bar startup

---
+++ src/components/navbar.jsx
@@ -0,0 +1,20 @@
+import "../index.css"
+
+function NavBar() {
+  return (
+    <header>
+      <nav>
+        <ul>
+          <li>
+            <a>Home</a>
+            <a>About</a>
+            <a>Projects</a>
+            <a>Contact Me</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  )
+}
+
+export default NavBar
