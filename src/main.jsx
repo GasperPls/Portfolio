db705364890cf09db0cca97fbfd02c6dd1edea37
GasperPls <rouni.assaf@outlook.com>
2026-01-16 22:07:20 -0800

    Testing playing with image, unfinished commit, cleaned some code, currently working in hero section for bio

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,14 @@ import './App.css'
 import App from './App.jsx'
 import Footer from './components/footer.jsx'
 import NavBar from './components/navbar.jsx'
+import Hero from './components/hero.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
     <NavBar />
+    <Hero />
     <Footer />
   </StrictMode>,
+
 )
